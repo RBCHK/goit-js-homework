e17225b91b5949ad3fecc977e2daffabd5d2d98b
RBCHK <artem.rybachuk@gmail.com>
2019-10-17 01:56:20 +0300

    1, 2, 3, 4?, 5

---
+++ goit-js-hw-05/index.js
@@ -0,0 +1,296 @@
+'use strict'
+
+//! Задание 1 ---------------------------------
+
+// Напиши функцию - конструктор Account, которая создает объект со свойствами
+// login и email.В prototype функции - конструктора добавь метод getInfo(),
+// который выводит в консоль значения полей login и email объекта который
+// его вызвал.
+
+//! Решение 1
+
+// function Account(login, email) {
+// 	this.login = login;
+// 	this.email = email;
+// }
+
+// Account.prototype.getInfo = function() {
+// 	return `login: ${this.login}, email: ${this.email}`
+// }
+
+// console.log(Account.prototype.getInfo); // function
+
+// const mango = new Account('Mangozedog', 'mango@dog.woof');
+// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+// console.log('mango.getInfo()', mango.getInfo())
+
+// const poly = new Account('Poly', 'poly@mail.com');
+// poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+// console.log('poly.getInfo()', poly.getInfo())
+
+//! Задание 2 ---------------------------------------
+
+// Напиши класс User для создания пользователя со следующим свойствами:
+
+// name - строка
+// age - число
+// followers - число
+
+// Добавь метод getInfo(), который, выводит строку: User ${имя} is ${возраст} 
+// years old and has ${кол-во фоловеров} followers
+
+//! Решение 2
+
+// class User {
+
+// 	constructor({name, age, followers}) {
+// 		this.name = name;
+// 		this.age = age;
+// 		this.followers = followers;
+// 	}
+
+// 	getInfo() {
+// 		console.log(
+// 			`User ${this.name} is ${this.age} years old and has ${this.followers} followers`
+// 		);
+// 	}
+// }
+
+// const mango = new User({name: 'Mango', age: 2, followers: 20});
+// mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+// const poly = new User({name: 'Poly', age: 3, followers: 17});
+// poly.getInfo(); // User Poly is 3 years old and has 17 followers
+
+
+//! Задание 3 ------------------------------
+
+// Напиши класс Storage, который будет создавать объекты для управления складом 
+// товаров. При вызове будет получать один аргумент - начальный массив товаров, 
+// и записывать его в свойство items.
+
+// Добавь методы класса:
+
+// getItems() - возвращает массив текущих товаров
+// addItem(item) - получает новый товар и добавляет его к текущим
+// removeItem(item) - получет товар и, если он есть, удаляет его из текущих
+
+//! Решение 3
+
+// class Storage {
+// 	constructor(items) {
+// 		this.items = items;
+// 	}
+
+// 	getItems() {
+// 		return this.items;
+// 	}
+
+// 	addItem(item) {
+// 		return this.items.push(item);
+// 	}
+
+// 	removeItem(item) {
+// 		if (this.items.include(item)) {
+// 			this.items.indexOf(item);
+
+// 		}
+// 	}
+// }
+
+// const storage = new Storage([
+// 	'Нанитоиды',
+// 	'Пролонгер',
+// 	'Железные жупи',
+// 	'Антигравитатор',
+// ]);
+
+// const items = storage.getItems();
+// console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+// storage.addItem('Дроид');
+// console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+// storage.removeItem('Пролонгер');
+// console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+
+//! Задание 4 -----------------------------
+
+// Напиши класс StringBuilder.На вход он получает один параметр - строку, которую
+// записывает в свойство _value.
+
+// Добавь классу следующий функционал:
+
+// 	Геттер value - возвращает текущее значение поля _value
+// Метод append(str) - получает парметр str(строку) и добавляет ее в конец _value
+// Метод prepend(str) - получает парметр str(строку) и добавляет ее в начало value
+// Метод pad(str) - получает парметр str(строку) и добавляет ее в начало и в конец _value
+
+//! Решение 4
+
+class StringBuilder {
+	constructor(value) {
+		this._value = value;
+	}
+
+	get value() {
+		return this._value;
+	}
+
+	set value(value) {
+		this._value = value;
+	}
+
+	append(str) {
+		// return this._value.splice(0, 0, str);
+		return this._value.push(str);
+	}
+
+	prepend(str) {
+		return this._value.unshift(str);
+	}
+
+	pad(str) {
+		return this._value.push(str) && this._value.unshift(str);
+	}
+}
+
+const builder = new StringBuilder(['.']);
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
+
+
+//! Задание 5 --------------------------------
+// Напиши класс Car с указанными свойствами и методами.
+
+//! Решение 5
+
+// class Car {
+// 	/*
+// 	 * Добавь статический метод `getSpecs(car)`,
+// 	 * который принимает объект-машину как параметр и выводит
+// 	 * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
+// 	 */
+// 	static getSpecs(car) {
+// 		const {
+// 			maxSpeed,
+// 			speed,
+// 			isOn,
+// 			distance,
+// 			price
+// 		} = car;
+// 		console.log(`maxSpeed: ${maxSpeed}, speed: ${speed}, isOn: ${isOn}, distance: ${distance}, price: ${price}`);
+// 	}
+
+// 	/*
+// 	 * Конструктор получает объект настроек.
+// 	 * Добавь свойства будущеего экземпляра класса:
+// 	 *  speed - текущая скорость, изначально 0
+// 	 *  price - цена автомобиля
+// 	 *  maxSpeed - максимальная скорость
+// 	 *  isOn - заведен ли автомобиль, значения true или false. Изначально false
+// 	 *  distance - общий киллометраж, изначально 0
+// 	 */
+// 	constructor({maxSpeed, price}) {
+// 		this.speed = 0;
+// 		this._price = price;
+// 		this.maxSpeed = maxSpeed
+// 		this.isOn = false;
+// 		this.distance = 0;
+// 	}
+
+// 	/*
+// 	 * Добавь геттер и сеттер для свойства price,
+// 	 * который будет работать с свойством цены автомобиля.
+// 	 */
+// 	get price() {
+// 		return this._price;
+// 	}
+
+// 	set price(price) {
+// 		return this._price = price;
+// 	}
+
+
+// 	/*
+// 	 * Добавь код для того чтобы завести автомобиль
+// 	 * Записывает в свойство isOn значение true
+// 	 */
+// 	//
+// 	turnOn(isOn) {
+// 		this.isOn = true;
+// 	}
+
+// 	/*
+// 	 * Добавь код для того чтобы заглушить автомобиль
+// 	 * Записывает в свойство isOn значение false,
+// 	 * и сбрасывает текущую скорость в 0
+// 	 */
+// 	turnOff(isOn) {
+// 		this.isOn = false;
+// 		this.speed = 0;
+// 	}
+
+// 	/*
+// 	 * Добавялет к свойству speed полученное значение,
+// 	 * при условии что результирующая скорость
+// 	 * не больше чем значение свойства maxSpeed
+// 	 */
+// 	accelerate(value) {
+
+// 		if (this.speed + value <= this.maxSpeed) {
+// 			this.speed += value;
+// 		}
+// 	}
+
+// 	/*
+// 	 * Отнимает от свойства speed полученное значение,
+// 	 * при условии что результирующая скорость не меньше нуля
+// 	 */
+// 	decelerate(value) {
+// 		if (this.speed - value >= 0) {
+// 			this.speed -= value;
+// 		}
+// 	}
+
+// 	/*
+// 	 * Добавляет в поле distance киллометраж (hours * speed),
+// 	 * но только в том случае если машина заведена!
+// 	 */
+// 	drive(hours) {
+// 		if (this.isOn) {
+// 			this.distance = hours * this.speed;
+// 		}
+// 	}
+// }
+
+// const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+// mustang.turnOn();
+// mustang.accelerate(50);
+// mustang.drive(2);
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+// mustang.decelerate(20);
+// mustang.drive(1);
+// mustang.turnOff();
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 0, isOn: false, distance: 30, price: 2000
+
+// console.log(mustang.price); // 2000
+
+// mustang.price = 4000;
+// console.log(mustang.price); // 4000
